5e6d89c3b06bc43f3f4ea36c2f39299efedf7862
Natwasa Paksrikul 6604062630196_Sec1 <152253951+6604062630196@users.noreply.github.com>
2026-01-16 18:21:49 +0700

    Add files via upload

--- sale.c
+++ sale.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main(){
-	int item1,item2,item3;
-	float vat = 1.07;
-	scanf("%d",&item1);
-    printf("%.2f\n", (item1 -(item1 * 0.05)) * vat );
-	scanf("%d",&item2);
-    printf("%.2f\n", ((item2+item1) - ((item2+item1) * 0.15)) * vat);
-	scanf("%d",&item3);
-    printf("%.2f\n", ((item1+item2+item3)  - ((item1+item2+item3) * 0.3))* vat );
+int main() {
+    float item1 , item2 , item3;
+
+    scanf("%f",&item1);
+    printf("%f\n",item1*0.95 );
+    scanf("%f",&item2);
+    printf("%f\n",(item1+item2)*0.85);
+    scanf("%f",&item3);
+    printf("%f",(item1+item2+item3)*0.7);
     return 0;
 }
